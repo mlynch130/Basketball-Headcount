f8f702bf7c788ff8c205be55826c68e06737e344
Mike Lynch <mjlynch@g.clemson.edu>
2017-07-04 13:38:50 -0500

    this is my test commit

--- PHP/admin.php
+++ PHP/admin.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!--This script will contain functions for getting admin settings -->
 
 <?php
@@ -27,34 +26,4 @@
         return $fileContents["CurrentHeadCountDate"];
     }
 
-=======
-<!--This script will contain functions for getting admin settings -->
-
-<?php
-
-    //###########
-    //NAME: getAdminFileContents
-    //DESCRIPTION: Loads the admin.json file into an array
-    //PARAMETERS:
-    //-(none)
-    //RETURNS: an array containing the admin.json contents
-    //###########
-    function getAdminFileContents() {
-        $fileContents = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT']."/Config/admin.json"),true);
-        return $fileContents;
-    }
-
-    //###########
-    //NAME: getCurrentHeadCountDate
-    //DESCRIPTION: returns the date for the current headcount
-    //PARAMETERS:
-    //-(none)
-    //RETURNS: the date in yyyy-mm-dd
-    //###########
-    function getCurrentHeadCountDate() {
-        $fileContents = getAdminFileContents();
-        return $fileContents["CurrentHeadCountDate"];
-    }
-
->>>>>>> basketball/master
 ?>
--- images/validatePlayerName.php
+++ images/validatePlayerName.php
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 <?php
 $playerName = '%'.$_POST['playerName'].'%';
 echo(false);
-=======
-<?php
-$playerName = '%'.$_POST['playerName'].'%';
-echo(false);
->>>>>>> basketball/master
 ?>
